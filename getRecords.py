001834e68853d140c284d777ce4fefbb001518df
Sarang-Abbasi <99014002+Sarang-Abbasi@users.noreply.github.com>
2025-07-24 04:55:32 +0500

    Delete getRecords.py

--- getRecords.py
+++
@@ -1,8 +0,0 @@
-from BlockchainConfig import get_total_records, get_signature_result
-
-if __name__ == "__main__":
-    total = get_total_records()
-    print(f"Total records: {total}")
-    for i in range(total):
-        rec = get_signature_result(i)
-        print(rec)
